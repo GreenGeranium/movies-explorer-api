71b9ac6acf70b531a59bde96274d6feb767175bb
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-27 18:40:32 +0300

    fix: исправить порядок лимитера

--- app.js
+++ app.js
@@ -41,21 +41,23 @@ app.use(express.json());
 
 // CORS
 app.use((req, res, next) => {
-  const { origin } = req.headers;
+  /* const { origin } = req.headers; */
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Credentials', true);
-  if (allowedCors.includes(origin)) {
+  /*  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-  }
+  } */
+
+  res.header('Access-Control-Allow-Origin', '*');
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
-  next();
+  return next();
 });
 
 app.use(requestLogger);
