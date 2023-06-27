b6785c0141afb9b6bee5d489835bbd902d6a9ed4
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-27 18:19:40 +0300

    fix: исправить порядок лимитера

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 
 // логирование
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -41,23 +42,7 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-// CORS
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-  }
-
-  next();
-});
+app.use(cors());
 
 app.use(limiter);
 
