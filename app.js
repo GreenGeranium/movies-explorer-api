dcb65745b5eafed950b1b9d836998928d415e618
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-27 17:17:51 +0300

    изменить порядок

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // подключение всех библиотек
-const express = require("express");
-const mongoose = require("mongoose");
-const { errors } = require("celebrate");
-const helmet = require("helmet");
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
 
 // логирование
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-const router = require("./routes");
-const { centralisedErrorHandler } = require("./errors/centralised-handler");
-const { limiter } = require("./middlewares/ratelimiter");
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const router = require('./routes');
+const { centralisedErrorHandler } = require('./errors/centralised-handler');
+const { limiter } = require('./middlewares/ratelimiter');
 
 const allowedCors = [
-  "http://localhost:4000",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "localhost:3000",
-  "http://api.geranius.nomoredomains.rocks",
-  "https://api.geranius.nomoredomains.rocks",
-  "http://geranius.nomoredomains.rocks",
-  "https://geranius.nomoredomains.rocks",
+  'http://localhost:4000',
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'localhost:3000',
+  'http://api.geranius.nomoredomains.rocks',
+  'https://api.geranius.nomoredomains.rocks',
+  'http://geranius.nomoredomains.rocks',
+  'https://geranius.nomoredomains.rocks',
 ];
 
 // запуск сервера с дефолтным портом 3000
@@ -29,33 +29,33 @@ app.use(helmet());
 
 // подключение к базе данных mestodb
 mongoose
-  .connect("mongodb://127.0.0.1:27017/bitfilmsdb")
+  .connect('mongodb://127.0.0.1:27017/bitfilmsdb')
   .then(() => {
-    console.log("Connecting mongo");
+    console.log('Connecting mongo');
   })
   .catch((err) => {
     console.log(err);
   });
 
-app.use(express.json());
-
 // CORS
 app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers["access-control-request-headers"];
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
   }
 
   next();
 });
+
+app.use(express.json());
 
 app.use(requestLogger);
 
