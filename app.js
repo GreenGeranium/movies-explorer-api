91ff77a81f21dc18ed3860580ba0c8aadf4a2e63
Maxim Grassi <grassimacsim@yandex.ru>
2023-06-27 18:17:27 +0300

    fix: исправить порядок лимитера

--- app.js
+++ app.js
@@ -39,16 +39,17 @@ mongoose
 
 app.use(express.json());
 
+app.use(requestLogger);
+
 // CORS
 app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  res.header('Access-Control-Allow-Origin', '*');
-  /*  if (allowedCors.includes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-  } */
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
@@ -57,8 +58,6 @@ app.use((req, res, next) => {
 
   next();
 });
-
-app.use(requestLogger);
 
 app.use(limiter);
 
